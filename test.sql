28a5297d69c67d0101eb58522cd9c9f987387550
Tatyana-Yam <129435785+Tatyana-Yam@users.noreply.github.com>
2023-08-10 17:47:40 +0300

    Add files via upload

--- test.sql
+++ test.sql
@@ -17,3 +17,54 @@ VALUES('Братья Карамазовы', 'Достоевский Ф.М.', 799
 
 select *
 from book;
+
+insert into book(title, author, price, amount)
+values('Игрок', 'Достоевский Ф.М.', 480.50, 10);
+insert into book(title, author, price, amount)
+values('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+SELECT distinct amount
+FROM book;
+
+insert into book(title, author, price, amount)
+values('Черный человек', 'Есенин С.А.', null, null);
+
+select author, count(author), count(amount), count(*)
+from book
+group by author;
+
+SELECT author AS Автор, COUNT(author) AS Различных_книг, SUM(amount) AS Количество_экземпляров
+FROM book
+GROUP BY author;
+
+SELECT author, MIN(price) AS Минимальная_цена, MAX(price) AS Максимальная_цена, round(AVG(price)) AS Средняя_цена
+FROM book
+GROUP BY author;
+
+SELECT author, sum(price*amount) AS Стоимость, ROUND((sum((price*amount)*0.18)/1.18), 2) AS НДС, round((sum(price*amount)/1.18), 2) AS Стоимость_без_НДС
+from book
+group by author;
+
+select MIN(price) AS Минимальная_цена, MAX(price) AS Максимальная_цена, ROUND(AVG(price), 2) AS Средняя_цена
+from book;
+
+select ROUND(AVG(price), 2) AS Средняя_цена, sum(price*amount) AS Стоимость
+from book
+GROUP BY amount
+HAVING amount between 5 and 14
+
+select author, sum(price*amount) AS Стоимость
+from book
+where title <> 'Идиот' and title <> 'Белая гвардия'
+group by author
+having sum(price*amount)>5000
+order by Стоимость desc;
+
+select author, price, amount, round((price*1.15), 2) AS new_price
+from book
+
+select author, sum(price*1.15*amount) AS planning_price
+from book
+where amount > 5
+group by author
+order by planning_price desc;
